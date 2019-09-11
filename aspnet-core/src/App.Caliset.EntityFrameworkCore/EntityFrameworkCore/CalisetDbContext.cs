0556bc76b11897d20df80ea40c2b35a6f3d06d2c
Matias Péres <matias@caliset.com>
2019-09-11 09:32:41 -0300

    - Se agrega OperationTypes al DbContext

--- aspnet-core/src/App.Caliset.EntityFrameworkCore/EntityFrameworkCore/CalisetDbContext.cs
+++ aspnet-core/src/App.Caliset.EntityFrameworkCore/EntityFrameworkCore/CalisetDbContext.cs
@@ -3,12 +3,13 @@ using Abp.Zero.EntityFrameworkCore;
 using App.Caliset.Authorization.Roles;
 using App.Caliset.Authorization.Users;
 using App.Caliset.MultiTenancy;
+using App.Caliset.Operations;
 
 namespace App.Caliset.EntityFrameworkCore
 {
     public class CalisetDbContext : AbpZeroDbContext<Tenant, Role, User, CalisetDbContext>
     {
-        /* Define a DbSet for each entity of the application */
+        public DbSet<OperationType> OperationTypes { get; set; }
         
         public CalisetDbContext(DbContextOptions<CalisetDbContext> options)
             : base(options)
