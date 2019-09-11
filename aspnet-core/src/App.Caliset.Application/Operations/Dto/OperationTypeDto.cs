4825267c33c99465bc1eb7fff1899360f664ac05
Matias Péres <matias@caliset.com>
2019-09-11 10:03:44 -0300

    Capa Aplicacion

---
+++ aspnet-core/src/App.Caliset.Application/Operations/Dto/OperationTypeDto.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Caliset.Operations.Dto
+{
+    [AutoMapFrom(typeof(OperationType))]
+    public class OperationTypeDto : EntityDto<int>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ aspnet-core/src/App.Caliset.Application/Operations/OperationTypeAppService.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using App.Caliset.Operations.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Caliset.Operations
+{
+    public class OperationTypeAppService : CrudAppService<OperationType, OperationTypeDto>
+    {
+        public OperationTypeAppService(IRepository<OperationType, int> repository) : base(repository)
+        {
+        }
+    }
+}
