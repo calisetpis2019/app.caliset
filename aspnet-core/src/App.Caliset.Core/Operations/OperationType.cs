7aed06132a3fca434ec25755a6fc7f927b667897
Matias Péres <matias@caliset.com>
2019-09-11 10:03:34 -0300

    - Se agrega auditing a OT

--- aspnet-core/src/App.Caliset.Core/Operations/OperationType.cs
+++ aspnet-core/src/App.Caliset.Core/Operations/OperationType.cs
@@ -1,9 +1,22 @@
-﻿using Abp.Domain.Entities;
+﻿using System;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using App.Caliset.Authorization.Users;
 
 namespace App.Caliset.Operations
 {
-    public class OperationType : Entity<int>
+    public class OperationType : Entity<int>, IFullAudited<User>, ISoftDelete
     {
         public string Name { get; set; }
+        public User CreatorUser { get; set; }
+        public User LastModifierUser { get; set; }
+        public long? CreatorUserId { get; set; }
+        public DateTime CreationTime { get; set; }
+        public long? LastModifierUserId { get; set; }
+        public DateTime? LastModificationTime { get; set; }
+        public User DeleterUser { get; set; }
+        public long? DeleterUserId { get; set; }
+        public DateTime? DeletionTime { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
