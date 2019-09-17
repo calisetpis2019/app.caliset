1f4ad713cb8d8cec9ace32fb5b8334298e0900fc
Eldkaii <54186786+Eldkaii@users.noreply.github.com>
2019-09-16 21:18:24 -0300

    Se comienza de 0 a implementar

--- aspnet-core/src/App.Caliset.EntityFrameworkCore/EntityFrameworkCore/CalisetDbContext.cs
+++ aspnet-core/src/App.Caliset.EntityFrameworkCore/EntityFrameworkCore/CalisetDbContext.cs
@@ -3,13 +3,12 @@ using Abp.Zero.EntityFrameworkCore;
 using App.Caliset.Authorization.Roles;
 using App.Caliset.Authorization.Users;
 using App.Caliset.MultiTenancy;
-using App.Caliset.Operations;
+
 
 namespace App.Caliset.EntityFrameworkCore
 {
     public class CalisetDbContext : AbpZeroDbContext<Tenant, Role, User, CalisetDbContext>
     {
-        public DbSet<OperationType> OperationTypes { get; set; }
         
         public CalisetDbContext(DbContextOptions<CalisetDbContext> options)
             : base(options)
---
+++ aspnet-core/src/App.Caliset.EntityFrameworkCore/Migrations/20190917001717_nuevo comienzo desde 0.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Caliset.Migrations
+{
+    public partial class nuevocomienzodesde0 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OperationTypes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OperationTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OperationTypes", x => x.Id);
+                });
+        }
+    }
+}
