9f604c3887ad4d13ed164082a8552f9b72c6d760
Salameboy <iamperdomo@gmail.com>
2019-10-02 20:43:27 -0300

    Fixes del Issue Alta Operacion: - Se pueden asignar los dos clientes (nominador y cargador) y la hora. - Se agregaron campos faltantes. - Se ve el estado en el listado de operaciones. Las ubicaciones aparecen por su nombre. -  Se cambio donde antes decia ubicación por operación.

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -52,3 +52,7 @@ Ajax.get('/AbpUserConfiguration/GetAll').then(data=>{
   }).$mount('#app')
 })
 
+import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
+import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
+
+Vue.component('VueCtkDateTimePicker', VueCtkDateTimePicker);
--- vue/src/views/operation/create-operation.vue
+++ vue/src/views/operation/create-operation.vue
@@ -1,50 +1,84 @@
 <template>
     <div>
-        <Modal :title="L('CreateNewOperation')"
+        <Modal :title="L('Crear Operación')"
                :value="value"
                @on-ok="save"
                @on-visible-change="visibleChange">
             <Form ref="operationForm" label-position="top" :rules="operationRule" :model="operation">
                 <Tabs value="detail">
                     <TabPane :label="L('Details')" name="detail">
-                        <FormItem :label="L('Date')" prop="date">
-                            <Input v-model="operation.date" type="date"></Input>
+
+                        <FormItem :label="L('Tipo')" prop="operationType">
+                            <Select v-model="operation.operationTypeId" style="padding: 10px 0px 20px 0px;" filterable placeholder="">
+                                <Option v-for="item in listOfOperationTypes" :value="item.id" :key="item.id">{{ item.name }}</Option>
+                            </Select>
                         </FormItem>
-                       <FormItem :label="L('Commodity')" prop="commodity">
+
+                        <FormItem :label="L('Nominador')" prop="nominator">
+                            <Select v-model="operation.nominatorId" style="padding: 10px 0px 20px 0px;" filterable placeholder="">
+                                <Option v-for="item in listOfClients" :value="item.id" :key="item.id">{{ item.name }}</Option>
+                            </Select>
+                        </FormItem>
+
+                        <FormItem :label="L('Cargador')" prop="loader">
+                            <Select v-model="operation.chargerId" style="padding: 10px 0px 20px 0px;" filterable placeholder="">
+                                <Option v-for="item in listOfClients" :value="item.id" :key="item.id">{{ item.name }}</Option>
+                            </Select>
+                        </FormItem>
+
+
+                        <FormItem :label="L('Lugar')" prop="location">
+                            <Select v-model="operation.locationId" style="padding: 10px 0px 20px 0px;" filterable placeholder="">
+                                <Option v-for="item in listOfLocations" :value="item.id" :key="item.id">{{ item.name }}</Option>
+                            </Select>
+                        </FormItem>
+
+                        <FormItem :label="L('Fecha y hora de inicio')" prop="date">
+                            <VueCtkDateTimePicker v-model="operation.date" locale="es" right=true />
+                        </FormItem>
+
+                        <FormItem :label="L('Responsable')" prop="manager">
+                            <Select v-model="operation.managerId" style="padding: 10px 0px 20px 0px;" filterable placeholder="">
+                                <Option v-for="item in listOfUsers" :value="item.id" :key="item.id">{{ item.name }}</Option>
+                            </Select>
+                        </FormItem>
+
+                        
+                        <FormItem :label="L('Mercaderia')" prop="commodity">
                             <Input v-model="operation.commodity" :maxlength="32"></Input>
                         </FormItem>
-                        <FormItem :label="L('Package')" prop="package">
+
+                        <FormItem :label="L('Paquete')" prop="package">
                             <Input v-model="operation.package" :maxlength="32"></Input>
                         </FormItem>
-                        <FormItem :label="L('Ship')" prop="ship">
+
+                        <FormItem :label="L('Nombre del Barco')" prop="ship">
                             <Input v-model="operation.shipName" :maxlength="32"></Input>
                         </FormItem>
-                        <FormItem :label="L('Destination')" prop="destination">
+
+                        <span>Destino</span>
+                        <FormItem :label="Destino" prop="destination">
                             <Input v-model="operation.destiny" :maxlength="32"></Input>
                         </FormItem>
-                        <FormItem :label="L('Line')" prop="line">
+
+                        <span>Referencia del Cliente</span>
+                        <FormItem :label="Cliente" prop="clientReference">
+                            <Input v-model="operation.clientReference" :maxlength="32"></Input>
+                        </FormItem>
+
+                        <span>Linea</span>
+                        <FormItem :label="Linea" prop="line">
                             <Input v-model="operation.line" :maxlength="32"></Input>
                         </FormItem>
-                        <FormItem :label="L('Booking')" prop="bookingNumber">
+
+                        <span>Numero del Booking</span>
+                        <FormItem :label="Booking" prop="bookingNumber">
                             <Input v-model="operation.bookingNumber" :maxlength="32"></Input>
                         </FormItem>
-                        <span>{{L('Location')}}</span>
-                        <Select :label="L('Location')" v-model="operation.idLocation" prop="location" style="padding: 10px 0px 20px 0px;">
-                            <Option v-for="item in listOfLocations" :value="item.id" :key="item.id">{{ item.id }}</Option>
-                        </Select>
-                        <span>{{L('Type')}}</span>
-                        <Select :label="L('Type')" v-model="operation.operationType" prop="operationType" style="padding: 10px 0px 20px 0px;">
-                            <Option v-for="item in listOfOperationTypes" :value="item.id" :key="item.id">{{ item.name }}</Option>
-                        </Select>
-                        <span>{{L('State')}}</span>
-                        <Select :label="L('State')" v-model="operation.operationState" prop="operationState" style="padding: 10px 0px 20px 0px;">
-                            <Option v-for="item in listOfOperationStates" :value="item.id" :key="item.id">{{ item.name }}</Option>
-                        </Select>
-                        <FormItem :label="L('Nominator')" prop="nominator">
-                            <Input v-model="operation.nominador" type="number"></Input>
-                        </FormItem>
-                        <FormItem :label="L('Loader')" prop="loader">
-                            <Input v-model="operation.cargador" type="number"></Input>
+                        
+                        <span>Notas</span>
+                        <FormItem :label="Notas" prop="notes">
+                            <Input v-model="operation.notes" :maxlength="32"></Input>
                         </FormItem>
                         
                     </TabPane>
@@ -62,28 +96,39 @@
     import { Component, Vue,Inject, Prop,Watch } from 'vue-property-decorator';
     import Util from '@/lib/util'
     import AbpBase from '@/lib/abpbase'
+    import PageRequest from '@/store/entities/page-request'
     import Operation from '@/store/entities/operation'
     import OperationState from '@/store/entities/operationState'
     import Location from '@/store/entities/location'
+
+    class PageCreateOperationRequest extends PageRequest {
+    }
 
     @Component
     export default class CreateOperation extends AbpBase{
         @Prop({type:Boolean,default:false}) value:boolean;
         operation:Operation=new Operation();
 
+        pagerequest: PageCreateOperationRequest = new PageCreateOperationRequest();
+
+
         get listOfLocations() {
             return this.$store.state.location.list;
-        };
-
-        get listOfOperationStates() {
-            console.log(this.$store.state.operationState.list);
-            return this.$store.state.operationState.list;
         };
 
         get listOfOperationTypes() {
             console.log(this.$store.state.operationType.list);
             return this.$store.state.operationType.list;
         };
+
+        get listOfClients() {
+            return this.$store.state.client.list;
+        };
+
+        get listOfUsers() {
+            return this.$store.state.user.list;
+        };
+
 
         save() {
             (this.$refs.operationForm as any).validate(async (valid:boolean)=>{
@@ -108,17 +153,73 @@
             if(!value){
                 this.$emit('input',value);
             }
+            this.getOperationTypes()
+            this.getClients()
+            this.getLocations()
+            this.getUsers()
         }
 
+        async getOperationTypes() {
+
+            await this.$store.dispatch({
+                type: 'operationType/getAll',
+                data: this.pagerequest
+            })
+
+        }
+
+        async getClients() {
+
+            await this.$store.dispatch({
+                type: 'client/getAll',
+                data: this.pagerequest
+            })
+
+        }
+
+        async getLocations() {
+
+            await this.$store.dispatch({
+                type: 'location/getAll',
+                data: this.pagerequest
+            })
+
+        }
+
+        async getUsers() {
+
+            await this.$store.dispatch({
+                type: 'user/getAll',
+                data: this.pagerequest
+            })
+
+        }
 
         operationRule={
-            date :[{required: true,message:this.L('FieldIsRequired',undefined,this.L('Date')),trigger: 'blur'}],
-            // nominator :[{required: true,message:this.L('FieldIsRequired',undefined,this.L('Nominator')),trigger: 'blur'}],
-            // loader :[{required: true,message:this.L('FieldIsRequired',undefined,this.L('Loader')),trigger: 'blur'}],
-            location :[{required: true,message:this.L('FieldIsRequired',undefined,this.L('Location')),trigger: 'blur'}],
-            commodity :[{required: true,message:this.L('FieldIsRequired',undefined,this.L('Commodity')),trigger: 'blur'}],
-            package :[{required: true,message:this.L('FieldIsRequired',undefined,this.L('Package')),trigger: 'blur'}] ,
-            operationType :[{required: true,message:this.L('FieldIsRequired',undefined,this.L('Type')),trigger: 'blur'}]
+            // operationType :[
+            //     { required: true,message:this.L('FieldIsRequired',undefined,this.L('Tipo')), trigger: 'blur' }
+            // ],
+            // nominator     :[
+            //     {required: true,message:this.L('FieldIsRequired',undefined,this.L('Nominador')),trigger: 'blur'}
+            // ],
+            // loader        :[
+            //     {required: true,message:this.L('FieldIsRequired',undefined,this.L('Cargador')),trigger: 'blur'}
+            // ],
+            // location      :[
+            //     {required: true,message:this.L('FieldIsRequired',undefined,this.L('Lugar')),trigger: 'blur'}
+            // ],
+            // date          :[
+            //     {required: true,message:this.L('FieldIsRequired',undefined,this.L('Fecha y hora')),trigger: 'blur'}
+            // ],
+            // manager       :[
+            //     {required: true,message:this.L('FieldIsRequired',undefined,this.L('Responsable')),trigger: 'blur'}
+            // ],
+            commodity     :[
+                {required: true,message:this.L('FieldIsRequired',undefined,this.L('Commodity')),trigger: 'blur'}
+            ],
+            package       :[
+                {required: true,message:this.L('FieldIsRequired',undefined,this.L('Package')),trigger: 'blur'}
+            ] 
         }
     }
 </script>
