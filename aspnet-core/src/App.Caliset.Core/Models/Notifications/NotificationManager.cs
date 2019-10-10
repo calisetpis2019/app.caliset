2adc1448cd72659bbc5cae5c7aaac0fea6f9fc4a
Victoria <victoria.gonbar@gmail.com>
2019-10-10 16:29:01 -0300

    Notificaciones al asignar usuario

--- aspnet-core/src/App.Caliset.Core/Models/Notifications/NotificationManager.cs
+++ aspnet-core/src/App.Caliset.Core/Models/Notifications/NotificationManager.cs
@@ -1,5 +1,6 @@
 ﻿using Abp.Domain.Repositories;
 using Abp.Domain.Services;
+using Abp.UI;
 using App.Caliset.Models.UserDeviceTokens;
 using System;
 using System.Collections.Generic;
@@ -20,31 +21,39 @@ namespace App.Caliset.Models.Notifications
         public void sendNotification(string Title,string Text, long UserId)
         {
 
+            var userToken = _repositoryUserDeviceTokens.FirstOrDefault(x => x.UserId == UserId);
+            if (userToken != null)
+            {
+                string deviceToken = userToken.DeviceToken;
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("https://fcm.googleapis.com/fcm/send");
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Headers.Add("Authorization: Key=AAAAEENtp_Q:APA91bEgvANU3DOnaMuFjJsr04LgEDPU2kE8cwyOS82oBfyJY9ewB8dA5fpGXOhtIpr0ZdoBR0IcylHnJQByo3j46oogpkfJjcfbde44GLQTkHOW1UOcA32o0zRM1DxMtOGMzTQ41s5f");
+                httpWebRequest.Method = "POST";
 
-            string deviceToken = _repositoryUserDeviceTokens.FirstOrDefault(x => x.UserId == UserId).DeviceToken;
 
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("https://fcm.googleapis.com/fcm/send");
-            httpWebRequest.ContentType = "application/json";
-            httpWebRequest.Headers.Add("Authorization: Key=AAAAEENtp_Q:APA91bEgvANU3DOnaMuFjJsr04LgEDPU2kE8cwyOS82oBfyJY9ewB8dA5fpGXOhtIpr0ZdoBR0IcylHnJQByo3j46oogpkfJjcfbde44GLQTkHOW1UOcA32o0zRM1DxMtOGMzTQ41s5f");
-            httpWebRequest.Method = "POST";
+                string cuerpo = "{\"notification\": { " +
+                                                                           "\"title\": \"" + Title + "\"," +
+                                                                           "\"text\": \"" + Text + "\"," +
+                                                                           "\"sound\": \"default\"" +
+                                                                           "}," +
+                                                                           "\"priority\": \"High\"," +
+                                                                           "\"to\": \"" + deviceToken +
+                                                                           "\"}";
 
+                byte[] postBytes = Encoding.UTF8.GetBytes(cuerpo);
 
-            string cuerpo = "{\"notification\": { " +
-                                                                       "\"title\": \"" + Title + "\"," +
-                                                                       "\"text\": \"" + Text + "\"," +
-                                                                       "\"sound\": \"default\"" +
-                                                                       "}," +
-                                                                       "\"priority\": \"High\"," +
-                                                                       "\"to\": \"" + deviceToken +
-                                                                       "\"}";
+                Stream dataStream = httpWebRequest.GetRequestStream();
+                dataStream.Write(postBytes, 0, postBytes.Length);
+                dataStream.Close();
+                var response = (HttpWebResponse)httpWebRequest.GetResponse();
+                Console.WriteLine();
+            } else
+            {
+                throw new UserFriendlyException("No es posible enviar notificación.");
+            }
+            
 
-            byte[] postBytes = Encoding.UTF8.GetBytes(cuerpo);
-
-            Stream dataStream = httpWebRequest.GetRequestStream();
-            dataStream.Write(postBytes, 0, postBytes.Length);
-            dataStream.Close();
-            var response = (HttpWebResponse)httpWebRequest.GetResponse();
-            Console.WriteLine();
+            
         }
     }
 }
