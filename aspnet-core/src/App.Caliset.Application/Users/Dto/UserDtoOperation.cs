3e08e99268a23b5929bd315ca7682708c10af07b
Victoria <victoria.gonbar@gmail.com>
2019-10-11 13:09:10 -0300

    Telefono en usuario

--- aspnet-core/src/App.Caliset.Application/Users/Dto/UserDtoOperation.cs
+++ aspnet-core/src/App.Caliset.Application/Users/Dto/UserDtoOperation.cs
@@ -14,5 +14,6 @@ namespace App.Caliset.Users.Dto
         public string Name { get; set; }
         public string Surname { get; set; }
         public string EmailAddress { get; set; }
+        public string Phone { get; set; }
     }
 }
