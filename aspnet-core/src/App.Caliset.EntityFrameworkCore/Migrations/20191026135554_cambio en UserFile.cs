85ec04a766fca4416bae835cb9482cef861815b6
Eldkaii <54186786+Eldkaii@users.noreply.github.com>
2019-10-26 10:59:49 -0300

    Cambios en UserFile

---
+++ aspnet-core/src/App.Caliset.EntityFrameworkCore/Migrations/20191026135554_cambio en UserFile.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Caliset.Migrations
+{
+    public partial class cambioenUserFile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Path",
+                table: "UserFiles");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Photo",
+                table: "UserFiles",
+                nullable: false,
+                defaultValue: new byte[] {  });
+
+            migrationBuilder.UpdateData(
+                table: "OperationStates",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationTime",
+                value: new DateTime(2019, 10, 26, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "OperationStates",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationTime",
+                value: new DateTime(2019, 10, 26, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "OperationStates",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationTime",
+                value: new DateTime(2019, 10, 26, 0, 0, 0, 0, DateTimeKind.Local));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "UserFiles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Path",
+                table: "UserFiles",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "OperationStates",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationTime",
+                value: new DateTime(2019, 10, 23, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "OperationStates",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationTime",
+                value: new DateTime(2019, 10, 23, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "OperationStates",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationTime",
+                value: new DateTime(2019, 10, 23, 0, 0, 0, 0, DateTimeKind.Local));
+        }
+    }
+}
